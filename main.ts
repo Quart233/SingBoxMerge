9a6317928286334360df26d228767f0aadbe106e
kuaizi <qhethe525@gmail.com>
2025-06-17 20:21:29 +0800

    feat(main): Result within region grouped.

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import { createServer } from "node:http"
 import { ProviderFactory, Provider }  from "./types/provider.ts"
 
-const template_url = "http://127.0.0.1:8080/template.json";
-
 const queue: Promise<Provider>[] = [
   ProviderFactory.create("Provider Name", "Subscription URL"),
 ]
@@ -10,14 +8,10 @@ const queue: Promise<Provider>[] = [
 const server = createServer(async (req, client) => {
   const providers = await Promise.all(queue);
 
-  console.log(providers);
-
-  // Fetch template
-  const templateRes = await fetch(template_url);
-  const templateRaw = await templateRes.json();
+  const result = providers.map(p => p.groupByCountry())
 
   client.writeHead(200, { 'Content-Type': 'application/json' });
-  client.end(JSON.stringify(templateRaw, null, 2));
+  client.end(JSON.stringify(result, null, 2));
 });
 
 const PORT = 3000;
