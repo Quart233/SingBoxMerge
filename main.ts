41a2cd9b2cf1a87dd69776b443a81c337e330aa5
kuaizi <qhethe525@gmail.com>
2025-06-18 16:06:39 +0800

    feat(main): Output outbounds.

--- main.ts
+++ main.ts
@@ -5,13 +5,19 @@ const queue: Promise<Provider>[] = [
   ProviderFactory.create("Provider Name", "Subscription URL"),
 ]
 
+const internal = [
+  { "type": "direct", "tag": "direct" },
+  { "type": "block", "tag": "block" }
+]
+
 const server = createServer(async (req, client) => {
   const providers = await Promise.all(queue);
 
-  const result = providers.map(p => p.byFlags()).map(p => p.toConfig())
+  const country = providers.map(p => p.byFlags()).map(p => p.toConfig())
+  const endpoints = providers.map(p => p.toConfig())
 
   client.writeHead(200, { 'Content-Type': 'application/json' });
-  client.end(JSON.stringify(result, null, 2));
+  client.end(JSON.stringify([...internal, ...country.flat(), ...endpoints.flat()], null, 2));
 });
 
 const PORT = 3000;
