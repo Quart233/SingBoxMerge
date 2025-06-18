ffc0220b6335cbed0c5cd59df263eec54e9a46a9
kuaizi <qhethe525@gmail.com>
2025-06-18 17:26:51 +0800

    feat: Render rules outbound from template.

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { createServer } from "node:http"
 import { ProviderFactory, Provider }  from "./types/provider.ts"
+import template from "./templates/template.json" with { type: "json" }
+import { Protocol } from "./outbounds/base.ts";
 
 const queue: Promise<Provider>[] = [
-  ProviderFactory.create("Provider Name", "Subscription URL"),
+  ProviderFactory.create("Provider Name", "Subscription URL")
 ]
 
 const internal = [
@@ -14,10 +16,15 @@ const server = createServer(async (req, client) => {
   const providers = await Promise.all(queue);
 
   const country = providers.map(p => p.byFlags()).map(p => p.toConfig())
+  const rules = template.route.rules
+                     .filter(r => !internal.map(o => o.tag).includes(r.outbound))
+                     .map(r => ({ tag: r.outbound, type: Protocol.Selector, outbounds: country.flat().map(o => o.tag) }))
+
   const endpoints = providers.map(p => p.toConfig())
+  const outbounds = [...internal, ...rules, ...country.flat(), ...endpoints.flat()]
 
   client.writeHead(200, { 'Content-Type': 'application/json' });
-  client.end(JSON.stringify([...internal, ...country.flat(), ...endpoints.flat()], null, 2));
+  client.end(JSON.stringify(Object.assign(template, { outbounds }), null, 2));
 });
 
 const PORT = 3000;
