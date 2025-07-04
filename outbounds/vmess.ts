61302731c9a106c5a5119b9e077b63a528cb8e43
kuaizi <qhethe525@gmail.com>
2025-07-04 15:07:33 +0800

    feat(outbounds): Add vmess uri decode.

--- outbounds/vmess.ts
+++ outbounds/vmess.ts
@@ -1,25 +1,52 @@
+import { Buffer } from 'node:buffer';
+import { Protocol } from "./index.ts"
 import { Outbound, BaseConfig } from "./base.ts"
 
-export interface VmessConfig extends BaseConfig {
+export interface Config extends BaseConfig {
+	tag: string;
+	uuid: string;
 	server: string;
 	server_port: number;
+	security: string;
+	alter_id: number;
 	transport?: object;
 }
 
+export interface VmessConfig {
+	add: string;
+	ps: string;
+	id: string;
+	scy: string;
+	aid: number;
+	port: number;
+}
+
 export class Vmess extends Outbound {
-	constructor(config: VmessConfig) {
+	constructor(config: Config) {
 		config.transport = {}
 		super(config);
 		this.validate(config)
 	}
 
-	decode(uri: string) {
+	static decode(uri: string) {
 		// Todo: vmess outbound uri decode.
-		const instance = new Vmess()	
+		const base64 = uri.substring(8);
+	  const decoded = Buffer.from(base64, "base64").toString("utf8");
+	  const config: VmessConfig = JSON.parse(decoded);
+
+		const instance = new Vmess({
+			tag: config.ps,
+			type: Protocol.Vmess,
+			security: config.scy,
+			uuid: config.id,
+			server: config.add,
+			alter_id: Number(config.aid),
+			server_port: Number(config.port)
+		})
 		return instance;
 	}
 
-	override validate(config: VmessConfig) {
+	override validate(config: Config) {
     if (!config.server || !config.server_port) {
       throw new Error('Invalid vmess configuration: missing required fields');
     }
--- providers/v2.ts
+++ providers/v2.ts
@@ -8,11 +8,11 @@ export class V2 extends Base {
 	  const res = await fetch(f.url);
 	  const text = await res.text();
 	  const decoded = Buffer.from(text, "base64").toString("utf8");
-	  const list = decoded.split('\n').map(uri => uri.trim())
+	  const list = decoded.split('\n').filter(uri => uri).map(uri => uri.trim())
 
 	  console.log(list)
 
-	  // instance.outbounds = list.outbounds.map(uri => Vmess.decode(uri))
+	  instance.outbounds = list.map(uri => Vmess.decode(uri))
 
 	  return instance;
 	}
