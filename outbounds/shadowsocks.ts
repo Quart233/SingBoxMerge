3b19fcf86a3f4c8ac2e32cf6b759bd14f65591bd
kuaizi <qhethe525@gmail.com>
2025-07-18 00:05:26 +0800

    refactor(shadowsocks): Handle uri by URL.

--- outbounds/shadowsocks.ts
+++ outbounds/shadowsocks.ts
@@ -17,24 +17,16 @@ export class Shadowsocks extends Outbound {
   }
 
   static decode(uri: string) {
-    const url = uri.substring(5);
-    const userinfo = url.split("@");
-    const base64 = userinfo[0];
-
-    const encrytption = Buffer.from(base64, "base64").toString("utf8").split(":");
-
-    console.log(encrytption)
-
-    const endpoint = userinfo[1].split("#");
-    const server = endpoint[0].split(":");
+    const url = new URL(uri);
+    const encrytption = Buffer.from(url.username, "base64").toString("utf8").split(":");
 
     const instance = new Shadowsocks({
       type: Protocol.Shadowsocks,
-      tag: decodeURIComponent(endpoint[1]),
+      tag: decodeURIComponent(url.hash.slice(1)),
       method: encrytption[0],
       password: encrytption[1],
-      server: server[0],
-      server_port: Number(server[1])
+      server: url.hostname,
+      server_port: Number(url.port)
     })
     return instance;
   }
